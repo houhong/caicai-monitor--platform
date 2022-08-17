01b44b899d2ba77c1e0603ce05edaa3d06541f01
王孝权 <wxq@wangxiaoquan.com>
2022-08-17 11:46:43 +0800

    -- 初始化UI项目

---
+++ monitor-parent-ui/src/views/Home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Home-container">
+    <h2>Hello, caicai-monitor-parent</h2>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scope lang="less">
+.Home-container {
+  text-align: center;
+}
+</style>
